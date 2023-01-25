3bb6f107e61ba329c19f088ccb3acee298f6bbf8
Kathy-kay <adigunkafy27@gmail.com>
2023-01-25 16:29:46 +0100

    a todo app that has the functionality to add task,delete,edit and complete

---
+++ src/App.jsx
@@ -0,0 +1,142 @@
+import { useState } from 'react'
+import desktopDark  from '../src/assets/desktopDark.jpg'
+import mobileDark from '../src/assets/mobileDark.jpg'
+import sun from '../src/assets/icon-sun.svg'
+//import {BiTrashAlt}from 'react-icons/bi'
+import {VscEdit} from 'react-icons/vsc'
+
+import './App.css'
+
+function App() {
+    const [input, setInput] = useState('')
+    const [todoList, setTodoList] = useState([])
+    const [editId, setEditId] = useState(0)
+    const [complete, setComplete] = useState(false)
+
+    const updateTodo = (id, text) =>{
+      const updatedTodo = todoList.map((t) =>
+      t.id === id?{id, text}
+      :t
+      )
+      setTodoList(updatedTodo) 
+    }
+
+
+    const handleSubmit = (e) =>{
+      e.preventDefault(); 
+      if(editId){
+        let text = input
+        updateTodo(editId.id, text)
+        setEditId(0)
+        setInput('')
+        return
+      }
+     
+
+     if(input !== ''){
+      const newTask = {
+        id: Math.floor(Math.random() * 1000) + 1,
+        text: input,
+        completed: complete
+      }
+      setTodoList([...todoList, newTask])
+      
+     }
+      setInput('')
+      
+    }
+  
+
+    
+    //console.log(input)
+    const handleChange = (e) =>{
+      e.preventDefault();
+      setInput(e.target.value);
+    }
+
+    const handleDelete = (id) =>{
+        const newTodoList = todoList.filter((task) =>{
+          return task.id !== id;
+        })
+        setTodoList([...newTodoList]);
+    }
+
+    const handleEdit = (id) =>{
+      const editTodo = todoList.find((task) => {
+        return task.id === id;
+       
+      })
+      setInput(editTodo.text);
+      setEditId(editTodo);
+    }
+    const toggleCompleted = (id) =>{
+      const completeTodo = todoList.map((task) =>{
+        if(task.id === id){
+          task.completed = !task.completed
+         
+        }
+        return task
+      })
+      setTodoList(completeTodo);
+      console.log(completeTodo)
+    }
+
+
+  return (
+    <div className="App">
+      <picture className='picture'>
+        <source srcSet={desktopDark} media="(min-width:485px)"/>
+        <img src={mobileDark} alt="dark desktop image" />
+      </picture>
+        
+        <div className="todo-container">
+            <div className="todo">
+              <div className="heading">
+                <h1>TODO</h1>
+                <img src={sun} alt="sun icon" className='sun'/>
+              </div>
+              <form className="input" onSubmit={handleSubmit}>
+              <input type="text"
+                 id='todo-text'
+                 placeholder='Create a new todo...'
+                 value={input}
+                 name="text"
+                 onChange={handleChange}/>
+                 <button type='submit' className="add-task">{editId?"Edit":"Add"}</button>
+              </form>
+            
+            <div className="todo-list">
+              {todoList.map((task) =>{
+                return(
+                  <ul className="todo-item" 
+                  key={task.id}
+                   
+                  >
+                    <div className='check-task'>
+
+                      <button className={`btn ${task.completed?'active': null}`} onClick={() => toggleCompleted(task.id)}><span></span></button>
+                    {/*<input type="checkbox" 
+                    name='checkbox' 
+                    onChange={(e) => console.log(e.target.value)}
+                    checked={task.completed}
+                />*/}
+                    <li className='list'>{task.text}</li>
+                    </div>
+                    <div className="button">
+                      <button
+                      onClick={() => handleEdit(task.id)}><VscEdit/></button>
+                      <button
+                      onClick={() => handleDelete(task.id)}>X</button>
+                    </div>
+                  </ul>
+                )
+              })}
+            </div>
+            </div>
+        </div>
+       
+    </div>
+  )
+}
+
+export default App

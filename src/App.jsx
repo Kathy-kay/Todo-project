1f906f2196cf2607b667b8206e605c4a0cb861b4
Kathy-kay <adigunkafy27@gmail.com>
2023-01-28 12:31:00 +0100

    implemented dark and light mode theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import desktopDark  from '../src/assets/desktopDark.jpg'
 import mobileDark from '../src/assets/mobileDark.jpg'
 import sun from '../src/assets/icon-sun.svg'
-//import {BiTrashAlt}from 'react-icons/bi'
+import moon from '../src/assets/icon-moon.svg'
+//import {BiTrashAlt}from 'react-icons/bi'/
 import {VscEdit} from 'react-icons/vsc'
 
 import './App.css'
@@ -12,6 +13,14 @@ function App() {
     const [todoList, setTodoList] = useState([])
     const [editId, setEditId] = useState(0)
     const [complete, setComplete] = useState(false)
+    const [lightMode, setLightMode] = useState(false)
+
+
+
+    const switchTheme = () =>{
+      const newTheme = lightMode? 'light': 'dark';
+      setLightMode(newTheme)
+    }
 
     const updateTodo = (id, text) =>{
       const updatedTodo = todoList.map((t) =>
@@ -83,17 +92,29 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App" data-theme={lightMode}>
       <picture className='picture'>
         <source srcSet={desktopDark} media="(min-width:485px)"/>
-        <img src={mobileDark} alt="dark desktop image" />
+        <img src={mobileDark} 
+        alt="dark desktop image"
+        onClick={switchTheme} />
       </picture>
         
         <div className="todo-container">
             <div className="todo">
               <div className="heading">
                 <h1>TODO</h1>
-                <img src={sun} alt="sun icon" className='sun'/>
+                {lightMode ?
+                <img src={moon} 
+                alt="moon icon"
+                className='sun'
+                onClick={switchTheme}/>
+                :
+                <img src={sun} 
+                alt="sun icon"
+                className='sun'
+                onClick={switchTheme}/>
+                }
               </div>
               <form className="input" onSubmit={handleSubmit}>
               <input type="text"
@@ -123,7 +144,7 @@ function App() {
                     <li className='list'>{task.text}</li>
                     </div>
                     <div className="button">
-                      <button
+                      <button 
                       onClick={() => handleEdit(task.id)}><VscEdit/></button>
                       <button
                       onClick={() => handleDelete(task.id)}>X</button>
